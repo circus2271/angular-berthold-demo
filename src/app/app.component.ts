6a28d47c79561e608d3cbbdead64b308c0761ffa
mundisyum <{ID}+{username}@users.noreply.github.com>
2023-01-05 04:04:03 +0300

    Change index.html; lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationStart, Router, Scroll } from '@angular/router';
-import { filter } from 'rxjs';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
@@ -13,28 +12,6 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
-    // this.router.events
-    //   .pipe(
-    //     filter(
-    //       (e) => e instanceof NavigationStart && this.mobileNavbarOpen === true
-    //     )
-    //   )
-    //   .subscribe(() => (this.mobileNavbarOpen = false));
-
-    // this.router.events
-    //   .pipe(filter((e) => e instanceof Scroll))
-    //   .subscribe((e) => {
-    //     const anchor = (e as Scroll).anchor;
-    //     if (anchor) {
-    //       const element = document.getElementById(anchor);
-    //       if (element) element.scrollIntoView({ behavior: 'smooth' });
-    //     }
-
-    //     if (!anchor) {
-    //       scrollTo(0, 0);
-    //     }
-    //   });
-
     this.router.events.subscribe((e) => {
       if (e instanceof NavigationStart && this.mobileNavbarOpen) {
         this.mobileNavbarOpen = false;
